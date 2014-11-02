f174b89ef75f91879f443335bae23322dee0ac00
kinglancer <kinglancer@163.com>
2014-11-02 19:49:07 +0000

    Update Main.hs
    This is a demo to show how we get user's input to configure our simulation. To make it practical, we need to design our simulation tests and decide which parts should be arguments configured by user when the program starts.

--- baseversion/Main.hs
+++ baseversion/Main.hs
@@ -3,8 +3,18 @@ import TestHarness
 
 
 main::IO()
-main = testmeHetero       --add test here
---main = testmeHeteroPairs
+main = do                                      --this is an example to show how to get user's input to configure the simulation.
+       putStrLn "We are about to start testmeHetero simulation"
+       putStrLn "Please configure the initial inventry of HFT agents.Such as [3000,0,3000] "
+       input <- getLine
+       
+       let invcfg :: [Double]                 -- use 'read' to parse the input,  
+           invcfg = (read input)
+           
+       putStrLn $"The number of Hft agents is " ++ (show $length invcfg)
+       putStrLn "testmeHetero starts"
+       hftwaveHD 2700 invcfg 1 1
+       putStrLn "testmeHetero ends"
 
 
 
